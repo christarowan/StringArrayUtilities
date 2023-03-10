4e82067fb0af7a95ee89c64437badcf22bfb61c1
Christa <christa@Zipcoders-MBP-6.lan>
2023-03-09 23:11:59 -0500

    Current progress.

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+import java.util.*;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -9,6 +12,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
+
         return array[0];
     }
 
@@ -17,6 +21,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+
         return array[1];
     }
 
@@ -25,15 +30,18 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 1];
     }
 
     /**
      * @param array array of String objects
      * @return second to last element in specified array
      */ // TODO
-    public static String getSecondToLastElement(String[] array) {
-        return null;
+
+        public static String getSecondToLastElement(String[] array) {
+
+            return array[array.length-2];
     }
 
     /**
@@ -42,6 +50,13 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for(String position : array){
+            if (position.equals(value)){
+
+
+            return true;
+            }
+        }
         return false;
     }
 
@@ -50,7 +65,20 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] result = new String[array.length];
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            String reverse = array[left];
+            array[left] = array[right];
+            array[right] = reverse;
+            left++;
+            right--;
+        }
+        return array;
+
+
     }
 
     /**
@@ -58,7 +86,15 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
+        String[] arr = array.clone();       //still playing with this to figure out how to get the correct result using .copy
+        String[] reverse =(reverse(arr));
+
+        if(Arrays.equals(reverse, array)) {
+            return true;
+        }
         return false;
+
+
     }
 
     /**
@@ -66,6 +102,22 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        Set<Character> alphaSet = new HashSet<Character>();
+        for (char a : alphabet) {
+            alphaSet.add(a);
+        }
+
+        Set<Character> inputSet = new HashSet<Character>();
+        for(String s : array){
+            for(char c : s.toCharArray()) {     // look at each character of array and evaluate whether every letter of
+                if (c != ' ') { inputSet.add(Character.toLowerCase(c)); }
+            }
+        }
+
+        if (alphaSet.equals(inputSet)) {
+            return true;
+        }
         return false;
     }
 
